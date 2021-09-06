629c3c57d3ad732c33df85ea2f770f76e0da7f44
tonocp <amcp88@gmail.com>
2021-09-06 08:18:14 +0200

    Formularios App

---
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent {
+
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    }
+  ];
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    }
+  ];
+
+
+}
